332b1f947b1942fbe1fe7fab24e9a7ca7c7c3ab5
Max Lynch <max@drifty.com>
2019-07-26 16:32:33 -0500

    Search demo

--- src/components/searchbar/searchbar.tsx
+++ src/components/searchbar/searchbar.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Element } from '@stencil/core';
+import { Component, h, Element, State } from '@stencil/core';
 
 @Component({
   tag: 'component-searchbar',
@@ -9,15 +9,51 @@ export class searchbar {
 
   list: HTMLElement;
 
+  cities = [
+    'Amsterdam',
+    'Bogota',
+    'Buenos Aires',
+    'Cairo',
+    'Dhaka',
+    'Edinburgh',
+    'Geneva',
+    'Genoa',
+    'Glasglow',
+    'Hanoi',
+    'Hong Kong',
+    'Islamabad',
+    'Istanbul',
+    'Jakarta',
+    'Kiel',
+    'Kyoto',
+    'Le Havre',
+    'Lebanon',
+    'Lhasa',
+    'Lima',
+    'London',
+    'Los Angeles',
+    'Madrid',
+    'Manila',
+    'New York',
+    'Olympia',
+    'Oslo',
+    'Panama City',
+    'Peking',
+    'Philadelphia',
+    'San Francisco',
+    'Seoul',
+    'Taipeh',
+    'Tel Aviv',
+    'Tokio',
+    'Uelzen',
+    'Washington',
+  ]
+
+  @State() items = this.cities;
+
   handleSearch = (event: any) => {
-    const items = Array.from(this.el.querySelector('ion-list').children);
     const query = event.target.value.toLowerCase();
-    requestAnimationFrame(() => {
-      items.forEach((item: HTMLElement) => {
-        const shouldShow = item.textContent.toLowerCase().indexOf(query) > -1;
-        item.style.display = shouldShow ? 'block' : 'none';
-      });
-    });
+    this.items = this.cities.filter(x => x.toLocaleLowerCase().indexOf(query) >= 0);
   }
 
   render() {
@@ -35,44 +71,14 @@ export class searchbar {
       </ion-header>,
 
       <ion-content>
-        <ion-list ref={e => this.list = e}>
-          <ion-item>Amsterdam</ion-item>
-          <ion-item>Bogota</ion-item>
-          <ion-item>Buenos Aires</ion-item>
-          <ion-item>Cairo</ion-item>
-          <ion-item>Dhaka</ion-item>
-          <ion-item>Edinburgh</ion-item>
-          <ion-item>Geneva</ion-item>
-          <ion-item>Genoa</ion-item>
-          <ion-item>Glasglow</ion-item>
-          <ion-item>Hanoi</ion-item>
-          <ion-item>Hong Kong</ion-item>
-          <ion-item>Islamabad</ion-item>
-          <ion-item>Istanbul</ion-item>
-          <ion-item>Jakarta</ion-item>
-          <ion-item>Kiel</ion-item>
-          <ion-item>Kyoto</ion-item>
-          <ion-item>Le Havre</ion-item>
-          <ion-item>Lebanon</ion-item>
-          <ion-item>Lhasa</ion-item>
-          <ion-item>Lima</ion-item>
-          <ion-item>London</ion-item>
-          <ion-item>Los Angeles</ion-item>
-          <ion-item>Madrid</ion-item>
-          <ion-item>Manila</ion-item>
-          <ion-item>New York</ion-item>
-          <ion-item>Olympia</ion-item>
-          <ion-item>Oslo</ion-item>
-          <ion-item>Panama City</ion-item>
-          <ion-item>Peking</ion-item>
-          <ion-item>Philadelphia</ion-item>
-          <ion-item>San Francisco</ion-item>
-          <ion-item>Seoul</ion-item>
-          <ion-item>Taipeh</ion-item>
-          <ion-item>Tel Aviv</ion-item>
-          <ion-item>Tokio</ion-item>
-          <ion-item>Uelzen</ion-item>
-          <ion-item>Washington</ion-item>
+        <ion-list>
+          {this.items.map(item => {
+            return (
+              <ion-item>
+                <ion-label>{item}</ion-label>
+              </ion-item>
+            );
+          })}
         </ion-list>
       </ion-content>
     ];
