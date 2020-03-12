1488e71ef2517a9db7f8dd46b5f2b6f5188530c7
Brandy Carney <brandy@ionic.io>
2020-03-11 20:40:43 -0400

    fix(icons): design improvements

--- src/components/icons/icons.tsx
+++ src/components/icons/icons.tsx
@@ -1,11 +1,19 @@
-import { Component, h } from '@stencil/core';
+import { Component, State, h } from '@stencil/core';
 
 @Component({
   tag: 'component-icons',
   styleUrl: 'icons.css'
 })
 export class icons {
+  @State() segment = '';
+
+  updateSegment(ev) {
+    this.segment = ev.detail.value;
+  }
+
   render() {
+    const { segment } = this;
+
     return [
       <ion-header translucent={true}>
         <ion-toolbar>
@@ -14,58 +22,113 @@ export class icons {
           </ion-buttons>
           <ion-title>Icons</ion-title>
         </ion-toolbar>
+        <ion-toolbar>
+          <ion-segment value={segment} onIonChange={(ev) => this.updateSegment(ev)}>
+            <ion-segment-button value="-outline">Outline</ion-segment-button>
+            <ion-segment-button value="">Filled</ion-segment-button>
+            <ion-segment-button value="-sharp">Sharp</ion-segment-button>
+          </ion-segment>
+        </ion-toolbar>
       </ion-header>,
 
-      <ion-content fullscreen={true} class="ion-padding ion-text-center">
-        <ion-icon name="logo-ionic" color="primary"></ion-icon>
+      <ion-content fullscreen={true} class="ion-padding-top ion-text-center">
+        {/* Logo icons never change fill */}
+        <ion-icon name="logo-ionic"></ion-icon>
         <ion-icon name="logo-pwa"></ion-icon>
-        <ion-icon name="heart" color="danger"></ion-icon>
-        <ion-icon name="logo-ionitron" color="primary"></ion-icon>
+        <ion-icon name="logo-ionitron"></ion-icon>
+        <ion-icon name="logo-javascript"></ion-icon>
+        <ion-icon name="logo-slack"></ion-icon>
+        <ion-icon name="logo-android"></ion-icon>
+        <ion-icon name="logo-apple"></ion-icon>
 
-        <ion-icon name="happy" color="warning"></ion-icon>
-        <ion-icon name="finger-print"></ion-icon>
-        <ion-icon name="person" color="tertiary"></ion-icon>
-        <ion-icon name="people-circle"></ion-icon>
+        {/* All other icons */}
+        <ion-icon name={`heart${segment}`}></ion-icon>
+        <ion-icon name={`happy${segment}`}></ion-icon>
+        <ion-icon name={`airplane${segment}`}></ion-icon>
+        <ion-icon name={`finger-print${segment}`}></ion-icon>
+        <ion-icon name={`person${segment}`}></ion-icon>
+        <ion-icon name={`people-circle${segment}`}></ion-icon>
+        <ion-icon name={`apps${segment}`}></ion-icon>
 
-        <ion-icon name="apps"></ion-icon>
-        <ion-icon name="lock-closed"></ion-icon>
-        <ion-icon name="key" color="success"></ion-icon>
-        <ion-icon name="attach"></ion-icon>
+        <ion-icon name={`lock-closed${segment}`}></ion-icon>
+        <ion-icon name={`key${segment}`}></ion-icon>
+        <ion-icon name={`attach${segment}`}></ion-icon>
+        <ion-icon name={`map${segment}`}></ion-icon>
+        <ion-icon name={`navigate${segment}`}></ion-icon>
+        <ion-icon name={`pin${segment}`}></ion-icon>
+        <ion-icon name={`locate${segment}`}></ion-icon>
 
-        <ion-icon name="map" color="secondary"></ion-icon>
-        <ion-icon name="navigate"></ion-icon>
-        <ion-icon name="pin"></ion-icon>
-        <ion-icon name="locate"></ion-icon>
+        <ion-icon name={`mic${segment}`}></ion-icon>
+        <ion-icon name={`volume-high${segment}`}></ion-icon>
+        <ion-icon name={`musical-notes${segment}`}></ion-icon>
+        <ion-icon name={`radio${segment}`}></ion-icon>
+        <ion-icon name={`cafe${segment}`}></ion-icon>
+        <ion-icon name={`calculator${segment}`}></ion-icon>
+        <ion-icon name={`bus${segment}`}></ion-icon>
 
-        <ion-icon name="mic"></ion-icon>
-        <ion-icon name="volume-high" color="danger"></ion-icon>
-        <ion-icon name="musical-notes"></ion-icon>
-        <ion-icon name="radio" color="primary"></ion-icon>
+        <ion-icon name={`wine${segment}`}></ion-icon>
+        <ion-icon name={`camera${segment}`}></ion-icon>
+        <ion-icon name={`star${segment}`}></ion-icon>
+        <ion-icon name={`image${segment}`}></ion-icon>
+        <ion-icon name={`pin${segment}`}></ion-icon>
+        <ion-icon name={`arrow-up-circle${segment}`}></ion-icon>
+        <ion-icon name={`arrow-undo${segment}`}></ion-icon>
 
-        <ion-icon name="cafe" color="success"></ion-icon>
-        <ion-icon name="calculator"></ion-icon>
-        <ion-icon name="bus" color="warning"></ion-icon>
-        <ion-icon name="wine"></ion-icon>
+        <ion-icon name={`chevron-down${segment}`}></ion-icon>
+        <ion-icon name={`play-circle${segment}`}></ion-icon>
+        <ion-icon name={`cloud${segment}`}></ion-icon>
+        <ion-icon name={`sunny${segment}`}></ion-icon>
+        <ion-icon name={`umbrella${segment}`}></ion-icon>
+        <ion-icon name={`rainy${segment}`}></ion-icon>
+        <ion-icon name={`cloud-done${segment}`}></ion-icon>
 
-        <ion-icon name="camera"></ion-icon>
-        <ion-icon name="star" color="tertiary"></ion-icon>
-        <ion-icon name="image"></ion-icon>
-        <ion-icon name="pin" color="secondary"></ion-icon>
+        <ion-icon name={`game-controller${segment}`}></ion-icon>
+        <ion-icon name={`alarm${segment}`}></ion-icon>
+        <ion-icon name={`barbell${segment}`}></ion-icon>
+        <ion-icon name={`earth${segment}`}></ion-icon>
+        <ion-icon name={`bicycle${segment}`}></ion-icon>
+        <ion-icon name={`basket${segment}`}></ion-icon>
+        <ion-icon name={`car${segment}`}></ion-icon>
 
-        <ion-icon name="arrow-up-circle" color="warning"></ion-icon>
-        <ion-icon name="arrow-undo"></ion-icon>
-        <ion-icon name="chevron-down"></ion-icon>
-        <ion-icon name="play-circle"></ion-icon>
+        <ion-icon name={`flash-off${segment}`}></ion-icon>
+        <ion-icon name={`gift${segment}`}></ion-icon>
+        <ion-icon name={`heart-half${segment}`}></ion-icon>
+        <ion-icon name={`ice-cream${segment}`}></ion-icon>
+        <ion-icon name={`leaf${segment}`}></ion-icon>
+        <ion-icon name={`paw${segment}`}></ion-icon>
+        <ion-icon name={`ribbon${segment}`}></ion-icon>
 
-        <ion-icon name="cloud"></ion-icon>
-        <ion-icon name="sunny" color="success"></ion-icon>
-        <ion-icon name="umbrella"></ion-icon>
-        <ion-icon name="rainy"></ion-icon>
+        <ion-icon name={`shirt${segment}`}></ion-icon>
+        <ion-icon name={`thumbs-up${segment}`}></ion-icon>
+        <ion-icon name={`warning${segment}`}></ion-icon>
+        <ion-icon name={`ice-cream${segment}`}></ion-icon>
+        <ion-icon name={`trash${segment}`}></ion-icon>
+        <ion-icon name={`tennisball${segment}`}></ion-icon>
+        <ion-icon name={`rose${segment}`}></ion-icon>
 
-        <ion-icon name="game-controller" color="danger"></ion-icon>
-        <ion-icon name="alarm"></ion-icon>
-        <ion-icon name="barbell"></ion-icon>
-        <ion-icon name="earth" color="primary"></ion-icon>
+        <ion-icon name={`subway${segment}`}></ion-icon>
+        <ion-icon name={`speedometer${segment}`}></ion-icon>
+        <ion-icon name={`egg${segment}`}></ion-icon>
+        <ion-icon name={`color-fill${segment}`}></ion-icon>
+        <ion-icon name={`cash${segment}`}></ion-icon>
+        <ion-icon name={`boat${segment}`}></ion-icon>
+        <ion-icon name={`aperture${segment}`}></ion-icon>
+
+        <ion-icon name={`albums${segment}`}></ion-icon>
+        <ion-icon name={`football${segment}`}></ion-icon>
+        <ion-icon name={`at-circle${segment}`}></ion-icon>
+        <ion-icon name={`bandage${segment}`}></ion-icon>
+        <ion-icon name={`barcode${segment}`}></ion-icon>
+        <ion-icon name={`bluetooth${segment}`}></ion-icon>
+        <ion-icon name={`bug${segment}`}></ion-icon>
+
+        <ion-icon name={`chatbox${segment}`}></ion-icon>
+        <ion-icon name={`code-working${segment}`}></ion-icon>
+        <ion-icon name={`cut${segment}`}></ion-icon>
+        <ion-icon name={`flask${segment}`}></ion-icon>
+        <ion-icon name={`hand-left${segment}`}></ion-icon>
+        <ion-icon name={`headset${segment}`}></ion-icon>
+        <ion-icon name={`library${segment}`}></ion-icon>
       </ion-content>
     ];
   }
