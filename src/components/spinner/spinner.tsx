fac1a8676370a4f7bd494f52aec0278376a68dfd
Brandy Carney <brandy@ionic.io>
2020-03-13 12:36:48 -0400

    feat(spinner): add example

--- src/components/spinner/spinner.tsx
+++ src/components/spinner/spinner.tsx
@@ -17,6 +17,62 @@ export class spinner {
       </ion-header>,
 
       <ion-content fullscreen={true}>
+        <ion-item>
+          <ion-spinner slot="start"></ion-spinner>
+          <ion-label>
+            Default Spinner
+          </ion-label>
+        </ion-item>
+
+        <ion-item>
+          <ion-spinner slot="start" name="lines"></ion-spinner>
+          <ion-label>
+            Lines
+          </ion-label>
+        </ion-item>
+
+        <ion-item>
+          <ion-spinner slot="start" name="lines-small"></ion-spinner>
+          <ion-label>
+            Lines Small
+          </ion-label>
+        </ion-item>
+
+        <ion-item>
+          <ion-spinner slot="start" name="dots"></ion-spinner>
+          <ion-label>
+            Dots
+          </ion-label>
+        </ion-item>
+
+        <ion-item>
+          <ion-spinner slot="start" name="bubbles"></ion-spinner>
+          <ion-label>
+            Bubbles
+          </ion-label>
+        </ion-item>
+
+        <ion-item>
+          <ion-spinner slot="start" name="circles"></ion-spinner>
+          <ion-label>
+            Circles
+          </ion-label>
+        </ion-item>
+
+        <ion-item>
+          <ion-spinner slot="start" name="crescent"></ion-spinner>
+          <ion-label>
+            Crescent
+          </ion-label>
+        </ion-item>
+
+        <ion-item>
+          <ion-spinner slot="start" paused></ion-spinner>
+          <ion-label>
+            Paused Spinner
+          </ion-label>
+        </ion-item>
+
       </ion-content>
     ];
   }
