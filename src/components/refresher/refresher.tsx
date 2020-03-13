3d2e729582b40d52d11cc3a550f9441e8ba32ba4
Brandy Carney <brandy@ionic.io>
2020-03-13 16:44:51 -0400

    fix(refresher): improve the refresher data to make a more realistic inbox

--- src/components/refresher/refresher.tsx
+++ src/components/refresher/refresher.tsx
@@ -1,25 +1,69 @@
 import { Component, h, State } from '@stencil/core';
 
-const names = [
-  'Burt Bear',
-  'Charlie Cheetah',
-  'Donald Duck',
-  'Eva Eagle',
-  'Ellie Elephant',
-  'Gino Giraffe',
-  'Isabella Iguana',
-  'Karl Kitten',
-  'Lionel Lion',
-  'Molly Mouse',
-  'Paul Puppy',
-  'Rachel Rabbit',
-  'Ted Turtle'
+const authors = [
+  'Everyworse',
+  'Nextably',
+  'Givester',
+  'TempleRentr',
+  'Spout',
+  'Frigital',
+  'Batterbus',
+  'Ham Pot',
+  'Bat+IQ',
+  'Throbe'
 ];
 
-const createItems = (count: number, unread: boolean) => {
+const titles = [
+  'Get 50% off your purchase today!',
+  'You\'re in luck, here\'s your Friday the 13th deal!',
+  'Don\'t miss out! Your exclusive access to FREE shipping is ending soon.',
+  'Treat yourself, you deserve these savings.',
+  'Get fresh meals delivered right to your door!',
+  'Your 25% Coupon Expires At Midnight',
+  'BIG SAVINGS Up To 50% Until Friday',
+  'Lucky you: Save today',
+  'The Event You Don\'t Want to Miss',
+  'Last Chance to Unlock Your 25% Code',
+  'CLOSEOUT SALE (We mean it!)',
+  'Gifting just got easier — enjoy exclusive access to FREE shipping for two days only.',
+  'Our Birthday = Your Savings',
+  'SPECIAL OFFER. See inside.',
+  'Ready to order?',
+  'BONUS Coupon Inside!',
+  'Today\'s SPECIAL BUY >> Inside >>',
+  'Email Exclusive Offer ENDS TONIGHT!'
+];
+
+const messages = [
+  'The best brands + the best deals',
+  'Recommended by your Personal Vacation Planner',
+  'Catch them before they\'re gone.',
+  'Shop by tonight to save on gifts to share with the entire family.',
+  'Over 30% OFF! View in Browser.',
+  'Offer expires 11:59 PM ET. View in Browser.',
+  'Free delivery will make us the MPV!',
+  'Selection √ | Value √ | Service √',
+  '⭐️⭐️⭐️⭐️⭐️',
+  'Shop now and save big...',
+  'Get the Lowest Price Guaranteed',
+  'Use code SAVE10 to save 10% on your next purchase.',
+  'Unlock your coupon code now...',
+  'Free Shipping Tonight!',
+  'Free Shipping On All Orders $35+',
+  'Better than takeout in every way',
+  'Get $100 off when you try now!',
+  'All your favorites are heading your way.',
+
+];
+
+const createItems = (count: number, unread = false) => {
   return new Array(count).fill(0).map(_ => {
     return {
-      name: names[Math.floor(Math.random() * names.length)],
+      email: {
+        author: authors[Math.floor(Math.random() * authors.length)],
+        title: titles[Math.floor(Math.random() * titles.length)],
+        message: messages[Math.floor(Math.random() * messages.length)],
+      },
       unread
     }
   });
@@ -33,12 +77,12 @@ const createItems = (count: number, unread: boolean) => {
 export class refresher {
   refresher: HTMLIonRefresherElement;
 
-  @State() items = createItems(5, false);
+  @State() items = createItems(5);
 
-  handleRefresh = () => {
+  handleRefresh = (ev: CustomEvent) => {
     setTimeout(() => {
       this.items = [...createItems(2, true), ...this.items];
-      this.refresher.complete();
+      (ev.target as HTMLIonRefresherElement).complete();
     }, 1000);
   }
 
@@ -54,17 +98,18 @@ export class refresher {
       </ion-header>,
 
       <ion-content fullscreen={true}>
-        <ion-refresher slot="fixed" id="refresher" onIonRefresh={this.handleRefresh} ref={e => this.refresher = e}>
+        <ion-refresher slot="fixed" onIonRefresh={ev => this.handleRefresh(ev)}>
           <ion-refresher-content></ion-refresher-content>
         </ion-refresher>
         <ion-list>
-          {this.items.map(({ unread, name }: any) => {
+          {this.items.map(({ unread, email }: any) => {
             return (
               <ion-item>
-                <div slot="start" class={unread ? 'dot unread' : 'dot read'}></div>
-                <ion-label class="ion-text-wrap">
-                  <h2>{name}</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                <div slot="start" class={unread ? 'dot dot-unread' : 'dot'}></div>
+                <ion-label>
+                  <h2>{email.author}</h2>
+                  <h3>{email.title}</h3>
+                  <p>{email.message}</p>
                 </ion-label>
               </ion-item>
             );
