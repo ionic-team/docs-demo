13cf969c55ca708bec0feba842d2dc7d3c039dda
Brandy Carney <brandy@ionic.io>
2020-03-23 13:15:57 -0400

    feat(nav): add nav demo

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -17,7 +17,50 @@ export class Nav {
       </ion-header>,
 
       <ion-content fullscreen={true}>
+        <div class="ion-padding">
+          <p>
+            Traditional web apps use a linear history, meaning that the user navigates
+            forward to a page and can hit the back button to navigate back. An example of
+            this is clicking around Wikipedia where the user is going forward and backward on
+            the browser's linear history stack.
+          </p>
 
+          <p>
+            In contrast, mobile apps often utilize parallel, "non-linear" navigation.
+            For example, a tabbed interface can have separate navigation stacks for each tab,
+            making sure the user never loses their place as they navigate and switch between
+            tabs.
+          </p>
+
+          <p>
+            Ionic apps embrace this mobile navigation approach, supporting parallel navigation
+            histories that can also be nested, all while maintaining the familiar browser-style
+            navigation concepts web developers are familiar with.
+          </p>
+
+          <p>
+            The implementation details for navigating in Ionic Framework varies between the
+            different frameworks. View the navigation guide for each framework below.
+          </p>
+        </div>
+
+        <ion-list>
+          <ion-item href="https://ionicframework.com/docs/angular/navigation" target="_blank">
+            <ion-label>
+              Angular Navigation
+            </ion-label>
+          </ion-item>
+          <ion-item href="https://ionicframework.com/docs/react/navigation" target="_blank">
+            <ion-label>
+              React Navigation
+            </ion-label>
+          </ion-item>
+          <ion-item href="https://ionicframework.com/docs/vue/navigation" target="_blank">
+            <ion-label>
+              Vue Navigation
+            </ion-label>
+          </ion-item>
+        </ion-list>
       </ion-content>
     ];
   }
