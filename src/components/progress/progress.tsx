4d0ed772b282eb8ea390348333ccc345ce27ccb6
Brandy Carney <brandy@ionic.io>
2020-03-24 12:37:35 -0400

    fix(progress): improve design

--- src/components/progress/progress.tsx
+++ src/components/progress/progress.tsx
@@ -5,6 +5,14 @@ import { Component, h } from '@stencil/core';
   styleUrl: 'progress.css'
 })
 export class Progress {
+  progressEl: HTMLIonProgressBarElement;
+
+  increaseValue() {
+    const value = this.progressEl.value;
+
+    this.progressEl.value = value + .1;
+  }
+
   render() {
     return [
       <ion-header translucent={true}>
@@ -13,30 +21,29 @@ export class Progress {
             <ion-back-button defaultHref="/"></ion-back-button>
           </ion-buttons>
           <ion-title>Progress</ion-title>
+          <ion-progress-bar type="indeterminate" color="dark"></ion-progress-bar>
         </ion-toolbar>
       </ion-header>,
 
       <ion-content fullscreen={true}>
         <ion-list>
           <ion-list-header>
-            <ion-label>Default</ion-label>
+            <ion-label>Determinate</ion-label>
           </ion-list-header>
-          <ion-progress-bar></ion-progress-bar>
+          <ion-progress-bar value={0.2}></ion-progress-bar>
         </ion-list>
 
         <ion-list>
           <ion-list-header>
-            <ion-label>Default at 50%</ion-label>
+            <ion-label>Control Determinate</ion-label>
+            <ion-button onClick={() => this.increaseValue()}>
+              <ion-icon slot="icon-only" ios="add-outline" md="add-sharp"></ion-icon>
+            </ion-button>
           </ion-list-header>
-          <ion-progress-bar value={0.5}></ion-progress-bar>
+          <ion-progress-bar ref={el => this.progressEl = el} value={0.1}></ion-progress-bar>
         </ion-list>
 
         <ion-list>
-          <ion-list-header>
-            <ion-label>Colors</ion-label>
-          </ion-list-header>
-          <ion-progress-bar color="secondary" value={0.5}></ion-progress-bar>
-          <ion-progress-bar color="tertiary" value={0.5}></ion-progress-bar>
 
           <ion-list-header>
             <ion-label>
@@ -58,6 +65,13 @@ export class Progress {
             </ion-label>
           </ion-list-header>
           <ion-progress-bar value={0.25} buffer={0.5}></ion-progress-bar>
+
+          <ion-list-header>
+            <ion-label>Colors</ion-label>
+          </ion-list-header>
+          <ion-progress-bar type="indeterminate" color="secondary" class="progress-margin"></ion-progress-bar>
+          <ion-progress-bar type="indeterminate" reversed={true} color="tertiary" class="progress-margin"></ion-progress-bar>
+          <ion-progress-bar value={0.25} buffer={0.5} color="danger" class="progress-margin"></ion-progress-bar>
         </ion-list>
       </ion-content>
     ];
