dccde90396aac18f0bc2d51e0c5ae25f100410b8
Liam DeBeasi <liamdebeasi@icloud.com>
2021-10-22 10:21:58 -0400

    update breadcrumbs demo

--- src/components/breadcrumbs/breadcrumbs.tsx
+++ src/components/breadcrumbs/breadcrumbs.tsx
@@ -6,18 +6,10 @@ import { BreadcrumbCustomEvent } from '@ionic/core';
   styleUrl: 'breadcrumbs.css'
 })
 export class Breadcrumb {
-  private popoverEl?: HTMLIonPopoverElement;
-
   @State() maxBreadcrumbs = 4;
 
   private expandBreadcrumbs = () => {
     this.maxBreadcrumbs = undefined;
-  }
-
-  private presentPopover = async (ev: BreadcrumbCustomEvent) => {
-    const { popoverEl } = this;
-
-    popoverEl.present(ev as any);
   }
 
   render() {
@@ -33,193 +25,7 @@ export class Breadcrumb {
           </ion-buttons>
           <ion-title>Breadcrumbs</ion-title>
         </ion-toolbar>
-      </ion-header>,
-
-      <ion-content fullscreen class="component-content">
-        <component-details description={description} url={url}></component-details>
-
-        <ion-list inset={true}>
-          <ion-list-header>Basic</ion-list-header>
-          <ion-breadcrumbs>
-            <ion-breadcrumb href="#">
-              Home
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#electronics">
-              Electronics
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#photography">
-              Photography
-            </ion-breadcrumb>
-          </ion-breadcrumbs>
-        </ion-list>
-
-        <ion-list inset={true}>
-          <ion-list-header>Colors</ion-list-header>
-          <ion-breadcrumbs color="secondary">
-            <ion-breadcrumb href="#">
-              Home
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#electronics">
-              Electronics
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#photography">
-              Photography
-            </ion-breadcrumb>
-          </ion-breadcrumbs>
-        </ion-list>
-
-        <ion-list inset={true}>
-          <ion-list-header>Icons - Start</ion-list-header>
-          <ion-breadcrumbs>
-            <ion-breadcrumb href="#">
-              <ion-icon slot="start" name="home"></ion-icon>
-              Home
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#files">
-              <ion-icon slot="start" name="folder"></ion-icon>
-              Files
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#projects">
-              <ion-icon slot="start" name="folder"></ion-icon>
-              Projects
-            </ion-breadcrumb>
-          </ion-breadcrumbs>
-        </ion-list>
-
-        <ion-list inset={true}>
-          <ion-list-header>Icons - End</ion-list-header>
-          <ion-breadcrumbs>
-            <ion-breadcrumb href="#">
-              Home
-              <ion-icon slot="end" name="home"></ion-icon>
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#files">
-              Files
-              <ion-icon slot="end" name="folder"></ion-icon>
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#projects">
-              Projects
-              <ion-icon slot="end" name="folder"></ion-icon>
-            </ion-breadcrumb>
-          </ion-breadcrumbs>
-        </ion-list>
-
-        <ion-list inset={true}>
-          <ion-list-header>Custom Separator - Icon</ion-list-header>
-          <ion-breadcrumbs>
-            <ion-breadcrumb href="#">
-              Home
-              <ion-icon slot="separator" name="arrow-forward"></ion-icon>
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#electronics">
-              Electronics
-              <ion-icon slot="separator" name="arrow-forward"></ion-icon>
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#photography">
-              Photography
-              <ion-icon slot="separator" name="arrow-forward"></ion-icon>
-            </ion-breadcrumb>
-          </ion-breadcrumbs>
-        </ion-list>
-
-        <ion-list inset={true}>
-          <ion-list-header>Max Items</ion-list-header>
-          <ion-breadcrumbs maxItems={4}>
-            <ion-breadcrumb href="#">
-              Home
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#electronics">
-              Electronics
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#photography">
-              Photography
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#cameras">
-              Cameras
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#film">
-              Film
-            </ion-breadcrumb>
-            <ion-breadcrumb>
-              35 mm
-            </ion-breadcrumb>
-          </ion-breadcrumbs>
-        </ion-list>
-
-        <ion-list inset={true}>
-          <ion-list-header>Items Before Collapse</ion-list-header>
-          <ion-breadcrumbs maxItems={4} itemsBeforeCollapse={2}>
-            <ion-breadcrumb href="#">
-              Home
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#electronics">
-              Electronics
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#photography">
-              Photography
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#cameras">
-              Cameras
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#film">
-              Film
-            </ion-breadcrumb>
-            <ion-breadcrumb>
-              35 mm
-            </ion-breadcrumb>
-          </ion-breadcrumbs>
-        </ion-list>
-
-        <ion-list inset={true}>
-          <ion-list-header>Items After Collapse</ion-list-header>
-          <ion-breadcrumbs maxItems={4} itemsAfterCollapse={3}>
-            <ion-breadcrumb href="#">
-              Home
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#electronics">
-              Electronics
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#photography">
-              Photography
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#cameras">
-              Cameras
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#film">
-              Film
-            </ion-breadcrumb>
-            <ion-breadcrumb>
-              35 mm
-            </ion-breadcrumb>
-          </ion-breadcrumbs>
-        </ion-list>
-
-        <ion-list inset={true}>
-          <ion-list-header>Items Before And After Collapse</ion-list-header>
-          <ion-breadcrumbs maxItems={4} itemsBeforeCollapse={0} itemsAfterCollapse={3}>
-            <ion-breadcrumb href="#">
-              Home
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#electronics">
-              Electronics
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#photography">
-              Photography
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#cameras">
-              Cameras
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#film">
-              Film
-            </ion-breadcrumb>
-            <ion-breadcrumb>
-              35 mm
-            </ion-breadcrumb>
-          </ion-breadcrumbs>
-        </ion-list>
-
-        <ion-list inset={true}>
-          <ion-list-header>Expand on Collapsed Indicator Click</ion-list-header>
+        <ion-toolbar>
           <ion-breadcrumbs maxItems={maxBreadcrumbs} onIonCollapsedClick={() => this.expandBreadcrumbs()}>
             <ion-breadcrumb href="#">
               Home
@@ -240,68 +46,11 @@ export class Breadcrumb {
               35 mm
             </ion-breadcrumb>
           </ion-breadcrumbs>
+          </ion-toolbar>
+      </ion-header>,
 
-          <ion-popover alignment="center" ref={el => this.popoverEl = el}>
-            <ion-content>
-              <ion-list>
-                <ion-item href="#electronics">
-                  <ion-label>Electronics</ion-label>
-                </ion-item>
-                <ion-item href="#photography">
-                  <ion-label>Photography</ion-label>
-                </ion-item>
-                <ion-item href="#cameras">
-                  <ion-label>Cameras</ion-label>
-                </ion-item>
-                <ion-item href="#film">
-                  <ion-label>Film</ion-label>
-                </ion-item>
-              </ion-list>
-            </ion-content>
-          </ion-popover>
-        </ion-list>
-
-        <ion-list inset={true}>
-          <ion-list-header>Popover on Collapsed Indicator Click</ion-list-header>
-          <ion-breadcrumbs maxItems={4} onIonCollapsedClick={(ev: BreadcrumbCustomEvent) => { this.presentPopover(ev) }}>
-            <ion-breadcrumb href="#">
-              Home
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#electronics">
-              Electronics
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#photography">
-              Photography
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#cameras">
-              Cameras
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#film">
-              Film
-            </ion-breadcrumb>
-            <ion-breadcrumb>
-              35 mm
-            </ion-breadcrumb>
-          </ion-breadcrumbs>
-        </ion-list>
-
-        <ion-list inset={true}>
-          <ion-list-header>Custom Separator - Icon</ion-list-header>
-          <ion-breadcrumbs>
-            <ion-breadcrumb href="#">
-              Home
-              <ion-icon slot="separator" name="arrow-forward"></ion-icon>
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#electronics">
-              Electronics
-              <ion-icon slot="separator" name="arrow-forward"></ion-icon>
-            </ion-breadcrumb>
-            <ion-breadcrumb href="#photography">
-              Photography
-              <ion-icon slot="separator" name="arrow-forward"></ion-icon>
-            </ion-breadcrumb>
-          </ion-breadcrumbs>
-        </ion-list>
+      <ion-content fullscreen class="component-content">
+        <component-details description={description} url={url}></component-details>
       </ion-content>
     ];
   }
