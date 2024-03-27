a173447c36e9a403b11d35a58a6a5211e88e7c99
Sean Perkins <sean@ionic.io>
2024-03-26 22:17:22 -0400

    feat(button): update button examples

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -17,37 +17,67 @@ export class Button {
       </ion-header>,
 
       <ion-content fullscreen={true}>
+
         <section>
           <ion-list-header>
             <ion-label>
-              Small Size
+              Colors
             </ion-label>
           </ion-list-header>
-          <ion-button size="small">Default</ion-button>
-          <ion-button size="small" color="secondary">Secondary</ion-button>
-          <ion-button size="small" color="tertiary">Tertiary</ion-button>
+          <div class="content">
+            <ion-button color="success">Success</ion-button>
+            <ion-button color="warning">Warning</ion-button>
+            <ion-button color="danger">Danger</ion-button>
+            <ion-button color="secondary">Secondary</ion-button>
+            <ion-button color="tertiary">Tertiary</ion-button>
+            <ion-button color="light">Light</ion-button>
+            <ion-button color="medium">Medium</ion-button>
+            <ion-button color="dark">Dark</ion-button>
+          </div>
         </section>
 
         <section>
           <ion-list-header>
             <ion-label>
-              Default Size
+              Sizes
             </ion-label>
           </ion-list-header>
-          <ion-button color="success">Success</ion-button>
-          <ion-button color="warning">Warning</ion-button>
-          <ion-button color="danger">Danger</ion-button>
+          <div class="content">
+            <ion-button size="small">Small</ion-button>
+            <ion-button>Default</ion-button>
+            <ion-button size="large">Large</ion-button>
+            <ion-button size="xlarge">XLarge</ion-button>
+          </div>
         </section>
 
         <section>
           <ion-list-header>
             <ion-label>
-              Large Size
+             Shapes
             </ion-label>
           </ion-list-header>
-          <ion-button size="large" color="light">Light</ion-button>
-          <ion-button size="large" color="medium">Medium</ion-button>
-          <ion-button size="large" color="dark">Dark</ion-button>
+          <div class="content">
+            <ion-button>Default</ion-button>
+            <ion-button shape="round">Round</ion-button>
+            <ion-button shape="rectangular">Rectangular</ion-button>
+            <ion-button shape="round">
+              <ion-icon slot="icon-only" icon="heart"></ion-icon>
+            </ion-button>
+          </div>
+        </section>
+
+        <section>
+          <ion-list-header>
+            <ion-label>
+             Fills
+            </ion-label>
+          </ion-list-header>
+          <div class="content">
+            <ion-button>Default</ion-button>
+            <ion-button fill="outline">Outline</ion-button>
+            <ion-button fill="solid">Solid</ion-button>
+            <ion-button fill="clear">Clear</ion-button>
+          </div>
         </section>
 
         <section>
